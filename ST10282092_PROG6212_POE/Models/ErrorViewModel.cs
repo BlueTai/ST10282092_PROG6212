3b6bd878cd05a5945337fd40c696dd28abd41224
bluetai <bluetaiphoon@gmail.com>
2025-09-17 20:34:25 +0200

    Dashboards for lecture and manger plus buttons work and basic dumy data to show that displaying history works

---
+++ ST10282092_PROG6212_POE/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ST10282092_PROG6212_POE.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
